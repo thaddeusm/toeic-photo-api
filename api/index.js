be2a706f3dc055d2dbb69b4befc7d43a7f5c3220
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2019-09-16 20:43:11 +0800

    Fix env

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const PexelsAPI = require('pexels-api-wrapper')
+const API_KEY = process.env.PEXEL_KEY
 
-var pexelsClient = new PexelsAPI(process.env.PEXEL_KEY);
+var pexelsClient = new PexelsAPI(API_KEY)
 
 module.exports = (req, res) => {
 
