7361e8222a41b3d4d8c4060321d4dc861f649470
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2019-09-16 21:02:23 +0800

    Add micro

--- api/index.js
+++ api/index.js
@@ -1,15 +1,23 @@
+const { send } = require('micro')
+const cors = require('micro-cors')()
+
 const PexelsAPI = require('pexels-api-wrapper')
 const API_KEY = process.env.PEXEL_KEY
 
 var pexelsClient = new PexelsAPI(API_KEY)
 
-module.exports = (req, res) => {
+const handler = (req, res) => {
+	if (req.method === 'GET') {
+		return send(res, 200, 'ok!')
+	}
 
 	pexelsClient.getCuratedPhotos(10, 1)
 	    .then(function(result){
-	        res.json(result);
+	        return res.status(200).json({"photos": result})
 	    }).
 	    catch(function(e){
-	        res.json(e);
+	    	return res.status(400).json({"error": e})
 	    });
 }
+
+module.exports = cors(handler)
