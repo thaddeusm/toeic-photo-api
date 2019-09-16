744cbe846a02ebcf929eca461f2adc75d6c5779f
Thaddeus McCleary <thaddeusmccleary@yahoo.com>
2019-09-16 21:04:54 +0800

    Fix micro config

--- api/index.js
+++ api/index.js
@@ -7,10 +7,6 @@ const API_KEY = process.env.PEXEL_KEY
 var pexelsClient = new PexelsAPI(API_KEY)
 
 const handler = (req, res) => {
-	if (req.method === 'GET') {
-		return send(res, 200, 'ok!')
-	}
-
 	pexelsClient.getCuratedPhotos(10, 1)
 	    .then(function(result){
 	        return res.status(200).json({"photos": result})
